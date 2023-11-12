07f954c4ba3abf7f4f322e3cdcf650652ada237b
snackierpirater <lolbitz2002@gmail.com>
2023-11-12 15:27:34 -0800

    initial commit

---
+++ src/main/java/com/snackpirate/tinkerswizardry/TinkersWizardry.java
@@ -0,0 +1,34 @@
+package com.snackpirate.tinkerswizardry;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TinkersWizardry.MOD_ID)
+public class TinkersWizardry
+{
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "tinkers_wizardry";
+
+    public TinkersWizardry()
+    {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
